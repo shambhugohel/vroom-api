d52739d41e458a215ae23e2974656875978168a1
Shambhu Gohel <gohelshambhu95@yahoo.com>
2025-11-22 00:11:10 +0530

    update keycloak

--- src/main/java/com/carportal/service/impl/CarDetailServiceImpl.java
+++ src/main/java/com/carportal/service/impl/CarDetailServiceImpl.java
@@ -1,28 +1,37 @@
 package com.carportal.service.impl;
 
+import com.carportal.api.rest.dto.CarDetailsDTO;
 import com.carportal.entity.ECarDetails;
 import com.carportal.repository.CarDetailRepository;
 import com.carportal.service.CarDetailService;
 import java.util.Collection;
 import java.util.Optional;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@RequiredArgsConstructor
 @Transactional(rollbackFor = Exception.class)
 public class CarDetailServiceImpl implements CarDetailService {
 
   private final CarDetailRepository carDetailRepository;
 
-  @Autowired
-  public CarDetailServiceImpl(CarDetailRepository carDetailRepository) {
-    this.carDetailRepository = carDetailRepository;
-  }
+//  @Override
+//  public ECarDetails saveCarDetail(CarDetailsDTO detailsDTO) {
+//    // get details from dto and create model Cardetailsmodel
+//    // check user role delaler, buyer, seller, agent, inspection,
+//    // car valuation
+//    // car inspection
+//    // Service history fetch
+//    // Accident history check
+//    return null;
+//  }
 
   @Override
-  public ECarDetails saveCarDetail(ECarDetails eCarDetails) {
-    return carDetailRepository.save(eCarDetails);
+  public ECarDetails saveCarDetail(ECarDetails ECarDetails) {
+    return null;
   }
 
   @Override
